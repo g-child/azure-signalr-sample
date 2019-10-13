c70058362c386bceeead4d82090234d83857e1e0
g.c <g.c@email.com>
2019-10-14 00:19:26 +0900

    initial commit

---
+++ SignalRServiceSample.Web/Models/Chat.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServiceSample.Web.Models
+{
+    public class Chat : Hub
+    {
+        public void BroadCastMessage(string name, string message)
+            => Clients.All.SendAsync("broadcastmessage", name, message);
+
+        public void Echo(string name, string message)
+            => Clients.Client(Context.ConnectionId).SendAsync("echo", name, message + " (echo from server)");
+    }
+}
---
+++ SignalRServiceSample.Web/Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SignalRServiceSample.Web.Models;
+
+namespace SignalRServiceSample.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddSignalR().AddAzureSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseFileServer();
+            app.UseAzureSignalR(routes => routes.MapHub<Chat>("/Chat"));
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
